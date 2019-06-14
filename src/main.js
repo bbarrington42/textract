03784bc70d0be89f84a924b36e390895256a571e
Bill Barrington <bill.barrington@candidpartners.com>
2019-06-14 09:33:55 -0500

    Move extractText into a module.

--- src/main.js
+++ src/main.js
@@ -9,39 +9,10 @@ const S = create ({
     env: env.concat (flutureEnv)
 });
 
-const Textract = require ('aws-sdk/clients/textract');
-const textract = new Textract ({
-    region: 'us-east-1'
-});
 
 const fs = require ('fs');
-
-const Future = require ('fluture');
-// const EOL = require ('os').EOL;
-//
-// const matcher = require ('./matcher');
+const {extractText} = require('../lib/extract');
 
 
-const detectText = buffer => Future ((reject, resolve) => {
+extractText(fs.readFileSync('../data/IMG_0171.JPG')).fork(console.error, console.log);
 
-    textract.detectDocumentText ({
-        Document: {
-            Bytes: buffer
-        }
-    }, (err, res) =>
-        err ? reject (err) : resolve (res));
-});
-
-detectText(fs.readFileSync('../data/IMG_0171.JPG')).fork(console.error, console.log);
-
-
-// const readFile = path => Future ((reject, resolve) => {
-//     fs.readFile (path, (err, res) => err ? reject (err) : resolve (res));
-// });
-//
-// const future = readFile ('../data/IMG_0171.JPG');
-//
-// const result = S.chain (detectText) (future);
-//
-//
-// result.fork (err => console.error(err.message), json => console.log(JSON.stringify(json)));
